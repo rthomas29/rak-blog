f832c299c001b0a04eef3a7ccc4ae9981474b075
Rakeem <thomas.rakeem.29@gmail.com>
2020-04-11 00:03:06 -0400

    eslint config changes

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -25,10 +25,6 @@ export const Sidebar = props => (
           siteMetadata {
             title
             subtitle
-            menu {
-              label
-              path
-            }
             author {
               name
               bio
